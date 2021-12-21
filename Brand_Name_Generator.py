fdc3faa6c1dbdca424adc32023748ae0fa67ee1a
Samra Qureshi <62799510+Samra-qur@users.noreply.github.com>
2021-12-21 22:35:38 +0530

    Add files via upload

---
+++ Brand_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator")
+city_name=input("What's the name of the city you grew up in?\n")
+pet_name=input("What is your pet's name?\n")
+print("Your brand name could be "+ city_name + " " + pet_name)
