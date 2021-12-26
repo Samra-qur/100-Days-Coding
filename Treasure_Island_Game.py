85634002e2187933bbe70153a602243d299ee891
Samra Qureshi <62799510+Samra-qur@users.noreply.github.com>
2021-12-26 19:16:43 +0530

    Add files via upload

---
+++ Treasure_Island_Game.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+count1=input("Welcome to the treasure Island. You are across the road. Where do you want to go?left or right\n ").lower()
+if count1=='left':
+  #Game Continues
+  count2=input("You have  came across lake. Do you want to swim or wait.\n").lower()
+  if count2=='swim':
+    print("You got eaten by crocodiles. Game Over !")
+  else:
+    count3=input("Waiting brings good news. We have found a house with 3 doors, Red, yellow or green. Choose to find treasure.\n").lower()
+    if count3=='red':
+      print("You entered room full of fire. Game over")
+    elif count3=='yellow':
+      print("You entered golden room. Congratulations for finding the treasure.")
+    elif count3=='green':
+      print("You got lost the woods. Game over !")
+    else:
+      print("Wrong choice of door. Game Over !")
+else:
+  #Game Over
+  print("You got biten by Lion. Game Over !")
+
+
+
