ddd43d100156e345ec73e74813eb4fbceb44f777
Samra Qureshi <62799510+Samra-qur@users.noreply.github.com>
2022-03-25 23:26:30 +0530

    Add files via upload

---
+++ TreasureIsland.py
@@ -0,0 +1,41 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+user_input=input("Do you want to turn left or right").lower()
+if user_input=='left':
+    user_input=input("You have reached a moutain full of wild animals. Do you want to move or stay").lower()
+    if user_input=='move':
+        user_input=input("You have came across a hut. There are three color gates: red, yellow, green. Choose one").lower()
+        if (user_input == 'red'):
+            print("You are captured! Game Over")
+        elif (user_input == 'green'):
+            print("You found the palace. Congrats you won !!")
+        else:
+            print("You reached house full of harmful gases. Game Over")
+    else:
+        print("You got caught by wild animals and You Loose")
+        
+else:
+    print("You Loose")
+#Write your code below this line 👇
